0171d7ab0fb1288d10bf891254f8784836bd0c55
Aram <aram.anto@gmail.com>
2024-11-05 10:38:43 +0100

    deleted unused router file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log("usernames will be logged here - wip");
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/new", function (req, res, next) {
+  console.log("username to be saved: ", req.body.username);
 });
 
 module.exports = router;
